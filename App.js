822a01f2e06e7e3537159e7e46a045270e0de207
Orson <orson.wan96@gmail.com>
2019-11-04 00:00:51 -0600

    Initialize

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '😊😭').join(' ')}
+        </Text>
+        <Text>This is a demo-page for React Native Project!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+
